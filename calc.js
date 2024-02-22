bfe095fce22f531a5a66b2383b709e593f9832e8
aoyama11 <157318787+aoyama11@users.noreply.github.com>
2024-02-22 15:11:30 -0500

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,2 +1,10 @@
-var result = document.getElementById("result")
+var result = document.getElementById("result");
+var clear = document.getElementByID("clear");
+
+clear.addEventListener("click", function (e) {
+  console.log(e.target.id)
+});
+  
+
+
 
